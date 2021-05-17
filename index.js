ef63ea06c07a382b337ad20710f69ac84072e6fc
catSIXe <github2.konrad@sch.umann.it>
2021-05-17 20:28:44 +0000

    first commit

--- index.js
+++ index.js
@@ -1,60 +1,90 @@
-const chalk = require('chalk')
-function comparePrint(shoudlbe, myimpl) {
-  let overeenstemming = 0
-  for (let i=0;i< shoudlbe.length;i++) {
-    if (myimpl[i] == shoudlbe[i]) overeenstemming++; else break;
-  }
-
-  console.log('  myimpl: ', chalk.greenBright(myimpl.substring(0, overeenstemming)) + chalk.red(myimpl.substring(overeenstemming)))
-  console.log('  should: ', chalk.blue(shoudlbe))
-  console.log()
-}
-
+const aesjs = require('./aes-64bit')
 const crc = require('node-crc')
 const crypto = require("crypto")
 //console.log(crypto.getCiphers())
-
-
-
-function getNullBytes(count) {
-  return '\x00'.repeat(count)
+function randomIntInc(low, high) {
+  return Math.floor(Math.random() * (high - low + 1) + low)
 }
-const TEST_KEY = '7C524984DA2A43607483ED3EFF92365FFF1C374024961F83CDA14A8DECD3D93A'.toLowerCase()
+class BOSkrypt {
+  constructor() {
+  }
+  //
+  encrypt(payload, key, keyIndex) {
+    key = Buffer.from(key, 'hex')
+    const iv = Buffer.from([ keyIndex, randomIntInc(0x00, 0xFF), randomIntInc(0x00, 0xFF) ])
+    const UNIXNOW = Math.floor(new Date().valueOf()/1e3)
+    const timestamp = (UNIXNOW - 1388534400).toString(16).padStart(2*4, '0')
+    const ivPadBuffer = Buffer.from("00".repeat(8), 'hex')
+    const _ivWithTimestamp = Buffer.from(timestamp + iv.toString('hex'), 'hex')
+    const _iv56bitCRC = crc.crc8(_ivWithTimestamp)
+    const ivWithTimestamp = Buffer.concat([
+      _ivWithTimestamp,
+      _iv56bitCRC
+    ])
+    const ivWithTimestampPadded = Buffer.concat([ ivWithTimestamp, ivPadBuffer ])
+    const uncPayloadHash = Buffer.from(crypto.createHash("sha1").update(payload).digest('hex').substring(0,10), 'hex') // first 40bits. 2*40/8
+
+    let preparedPayload = "ENCR" + payload + '\x00'.repeat(count) // random null-fill
+
+    let compressedPreparedPayload = preparedPayload.split('').map(x => { //foreach char run this
+      return x.charCodeAt(0) //get byte
+        .toString(2) // to base2/bcd/"binary"
+        .padStart(8, '0') // pad to 8 digits (dec4 = 100 => 00000100)
+        .split('') // to array
+        .reverse() // reverse
+        .join('') // back to string
+        .substring(0, 7) //take first 7 chars
+    }).join('') //join to one complete string
+
+    let compressedPayload = []
+    for (let i=0;i<compressedPreparedPayload.length;i+=8) {
+      let byte = parseInt(
+        compressedPreparedPayload
+        .substring(i, i+8) //select 8char chunk from bigstring
+        .split('') // to array
+        .reverse() // reverse
+        .join(''), // back to string
+      2) // parse from base2
+      compressedPayload.push( byte ) //put in array
+    }
+    compressedPayload = Buffer.from(compressedPayload)
+
+    const toEncryptData = Buffer.concat([
+      uncPayloadHash, compressedPayload
+    ])
+    let encryptedPayload = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(ivWithTimestampPadded)).encrypt(toEncryptData)
+    const encrpytedIncludingIVandSHA1Checksum = Buffer.concat([
+      ivWithTimestamp, encryptedPayload
+    ])
+    return encrpytedIncludingIVandSHA1Checksum.toString('base64')
+  }
+}
+/*
+const TEST_KEY = 'a568af91f233d50331142112e8a4dd457ff0aef8d79bfd9f50e338eec662160a'.toLowerCase()
 const key = Buffer.from(TEST_KEY, 'hex')
 
-const iv = 0x16FB7A1C
-const timestamp = 0x8E83DB0D
+const keyIndex = 0x16
+const iv = Buffer.from([keyIndex, 0xFB, 0x7A])
+console.log(iv)
+
+const UNIXNOW = Math.floor(new Date().valueOf()/1e3)
+// 1. Januar 2014 00:00:00 UTC
+const timestamp = (UNIXNOW - 1388534400).toString(16).padStart(2*4, '0')
+console.log('TS=', timestamp)
 const ivPadBuffer = Buffer.from("00".repeat(8), 'hex')
-const ivWithTimestamp = Buffer.from(timestamp.toString(16) + iv.toString(16), 'hex')
+const _ivWithTimestamp = Buffer.from(timestamp + iv.toString('hex'), 'hex')
+const _iv56bitCRC = crc.crc8(_ivWithTimestamp)
+const ivWithTimestamp = Buffer.concat([
+  _ivWithTimestamp,
+  _iv56bitCRC
+])
 const ivWithTimestampPadded = Buffer.concat([ ivWithTimestamp, ivPadBuffer ])
 // right hash
-const uncPayload = "Test"
+const uncPayload = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_Test_BOSKrypt_123"
 console.log(uncPayload.length)
 const uncPayloadHash = Buffer.from(crypto.createHash("sha1").update(uncPayload).digest('hex').substring(0,10), 'hex') // first 40bits. 2*40/8
 
-let preparedPayload = "ENCR" + uncPayload + getNullBytes(16) // random null-fill
-
-console.log('Key:')
-comparePrint(TEST_KEY, key.toString('hex'))
-
-console.log('IV with Timestamp:')
-comparePrint(
-  '8e83db0d16fb7a1c', // should
-  ivWithTimestamp.toString('hex') // is
-)
-
-console.log('SHA1(Cleartext):')
-comparePrint(
-  '640ab2bae0', //should
-  uncPayloadHash.toString('hex') // is
-)
-
-
-console.log('1) EncIndWithClearText', preparedPayload.length + ' bytes')
-comparePrint(
-  '454e43525465737400000000000000000000000000000000', // should 
-  Buffer.from(preparedPayload).toString('hex') // is
-)
+let preparedPayload = "ENCR" + uncPayload + getNullBytes(2) // random null-fill
 
 let compressedPreparedPayload = preparedPayload.split('').map(x => { //foreach char run this
   return x.charCodeAt(0) //get byte
@@ -65,8 +95,6 @@ let compressedPreparedPayload = preparedPayload.split('').map(x => { //foreach c
     .join('') // back to string
     .substring(0, 7) //take first 7 chars
 }).join('') //join to one complete string
-
-//compressedPreparedPayload += '0'.repeat(8-(compressedPreparedPayload.length % 8))
 
 let compressedPayload = []
 for (let i=0;i<compressedPreparedPayload.length;i+=8) {
@@ -84,47 +112,35 @@ compressedPayload = Buffer.from(compressedPayload)
 const toEncryptData = Buffer.concat([
   uncPayloadHash, compressedPayload
 ])
-console.log('2) EncIndWithClearTextCompressed', compressedPayload.length + ' bytes')
-comparePrint(
-  '45e7504a2dcfe90000000000000000000000000000', //should
-  compressedPayload.toString('hex') // is
-)
-
-console.log('toEncrypt', toEncryptData.length + ' bytes')
-comparePrint(
-  '640ab2bae0' + '45e7504a2dcfe90000000000000000000000000000', //should
-  toEncryptData.toString('hex') // is
-)
-
-let encryptedPayload = crypto.createCipheriv('aes-256-ctr', key, ivWithTimestampPadded, {
-}).update(toEncryptData)
-
-//encryptedPayload = Buffer.from('TSos4dWkdWCStRAUKsL74PGTYg5wDH3SAEA==','base64')
-
+let encryptedPayload = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(ivWithTimestampPadded)).encrypt(toEncryptData)
 const encrpytedIncludingIVandSHA1Checksum = Buffer.concat([
-  ivWithTimestamp,
-  encryptedPayload
+  ivWithTimestamp, encryptedPayload
 ])
-console.log('3) Encrypted including IV and SHA1 checksum CTR mode', encrpytedIncludingIVandSHA1Checksum.length + ' bytes')
-comparePrint(
-  Buffer.from('joPbDRb7ehxTSos4dWkdWCStRAUKsL74PGTYg5wDH3SAEA==','base64').toString('hex'), // should
-  encrpytedIncludingIVandSHA1Checksum.toString('hex') // is
-)
-
-// expected
-// 8 byte 64bit IV + 40byte Encrypted Payload(containg 34byte of something and 6byte of whatever)
-
-
-// 'joPbDRb7ehx' + 'TSos4dWkdWCStRAUKsL74PGTYg5wDH3SAEA=='
-// 'joPbDRb7ehxTSos4dWkdWCStRAUKsL74PGTYg5wDH3SAEA==' length 48
-// 8e83db0d16fb7a1c534a8b3875691d5824ad44050ab0bef83c64d8839c031f748010
-
-//
-
 
 let base64 = encrpytedIncludingIVandSHA1Checksum.toString('base64')
-console.log('4) base64', base64.length + ' bytes')
-comparePrint(
-  'joPbDRb7ehxTSos4dWkdWCStRAUKsL74PGTYg5wDH3SAEA==', // should
-  base64 // is
-)
+*/
+
+const	axios = require('axios')
+const config = {
+  "pager": {
+		"url": "http://127.0.0.1:3000/api/message/advanced",
+		"params": {
+			"type": "simple",
+			"routing": {
+				"device": "generic",
+				"connectors": [
+					[
+						"pocsag",
+						"42001A"
+					]
+				]
+			}
+		}
+	},
+}
+async function sendPage(payload) {
+	const meta = Object.assign({ ...config.pager.params }, { payload })
+	return (await axios.post(config.pager.url, meta)).data
+}
+
+sendPage(base64)
